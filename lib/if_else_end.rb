5082c614381e3e0001175dfd8bd47394da098db8
dependable-coder-f3d7 <tj.han@yale.edu>
2021-05-31 00:55:05 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,5 +1,10 @@
-# Write your solution here
+current_time = Time.now
+current_time = current_time.to_i
 
-if true 
-  puts "This code runs!"
+puts current_time
+
+if current_time % 2 == 0
+  puts "Even!"
+else
+  puts "Odd!"
 end
